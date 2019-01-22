bcae97af1aef2c2022356a6b2dedf63df380db8d
nov301996 <32534346+nov301996@users.noreply.github.com>
2019-01-22 12:00:31 +0530

    First Upload

---
+++ Scala Training/Test.scala
@@ -0,0 +1,8 @@
+class Test{
+var name="suraz"}
+
+object TestClient{
+def main(args:Array[String]){
+val t=new Test
+println(t.name)}
+}
---
+++ Scala Training/testing.scala
@@ -0,0 +1,7 @@
+class testing{
+var name="vaibhav19sahi"
+}
+object TestClient1 extends App{
+val t=new testing
+println(t.name)
+}
